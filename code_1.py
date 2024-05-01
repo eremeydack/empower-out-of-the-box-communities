0262c060637f97b888041253a2fddea022664987
eremeydack <139877570+eremeydack@users.noreply.github.com>
2024-05-01 19:23:44 +0200

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,25 @@
+        int[] data = new int[10];
+    public static void main(String[] args) {
+    }
+            data[i] = (int) (Math.random() * 100) + 1;
+}
+public class RandomDataGenerator {
+        }
+        for (int item : data) {
+
+        for (int i = 0; i < 10; i++) {
+        }
+
+            System.out.println("Random Number: " + item);            System.out.println("Random Number: " + item);
+    public static void main(String[] args) {
+        }
+    }
+}
+
+        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+
+        }
